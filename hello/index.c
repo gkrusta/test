3a3f8767903fc91ae5dea5928e0437a53cfba8fa
Gabriela <krustagabriela@gmail.com>
2023-10-18 17:29:02 +0200

    123

---
+++ hello/index.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 12:30:52 by gkrusta           #+#    #+#             */
+/*   Updated: 2023/10/18 17:28:58 by gkrusta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	parse_env(t_shell *shell, char **envp)
+{
+	var = ft_strddup((const char **)envp);
+	create_env_lst(shell, envp);
+}
+
+void	ft_leaks(void)
+{
+	system("leaks -q minishell");
+}
+
+void	free_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
